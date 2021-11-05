b6199bd5c0063d43e8798cebc2f99ec432417dfb
Marko Bisevac <markobisevac5@gmail.com>
2021-11-05 20:32:13 +0100

    commit

---
+++ 4coder_fleury_lang_list.h
@@ -0,0 +1,81 @@
+/* date = January 29th 2021 9:37 pm */
+
+#ifndef FCODER_FLEURY_LANG_LIST_H
+#define FCODER_FLEURY_LANG_LIST_H
+
+// NOTE(rjf): Include language files here.
+#include "generated/4coder_fleury_lexer_jai.h"
+#include "generated/4coder_fleury_lexer_jai.cpp"
+#include "generated/4coder_marko_lexer_odin.h"
+#include "generated/4coder_marko_lexer_odin.cpp"
+#include "4coder_fleury_lang_cpp.cpp"
+#include "4coder_fleury_lang_jai.cpp"
+#include "4coder_fleury_lang_metadesk.cpp"
+#include "4coder_marko_lang_odin.cpp"
+
+// NOTE(rjf): @f4_register_languages Register languages.
+function void
+F4_RegisterLanguages(void)
+{
+    // NOTE(rjf): C/C++
+    {
+        String_Const_u8 extensions[] =
+        {
+            S8Lit("cpp"), S8Lit("cc"), S8Lit("c"), S8Lit("cxx"),
+            S8Lit("C"), S8Lit("h"), S8Lit("hpp"),
+        };
+        for(int i = 0; i < ArrayCount(extensions); i += 1)
+        {
+            F4_RegisterLanguage(extensions[i],
+                                F4_CPP_IndexFile,
+                                lex_full_input_cpp_init,
+                                lex_full_input_cpp_breaks,
+                                F4_CPP_PosContext,
+                                F4_CPP_Highlight,
+                                Lex_State_Cpp);
+        }
+    }
+    
+    // NOTE(rjf): Jai
+    {
+        F4_RegisterLanguage(S8Lit("jai"),
+                            F4_Jai_IndexFile,
+                            lex_full_input_jai_init,
+                            lex_full_input_jai_breaks,
+                            F4_Jai_PosContext,
+                            F4_Jai_Highlight,
+                            Lex_State_Jai);
+    }
+    
+    // NOTE(rjf): Metadesk
+    {
+        String_Const_u8 extensions[] =
+        {
+            // TODO(rjf): Maybe find a config-driven way to specify these? "mc" was sort of
+            // introduced ad-hoc...
+            S8Lit("md"), S8Lit("mc"), S8Lit("metacode"), S8Lit("meta"), S8Lit("metadesk"),
+        };
+        for(int i = 0; i < ArrayCount(extensions); i += 1)
+        {
+            F4_RegisterLanguage(extensions[i],
+                                F4_MD_IndexFile,
+                                lex_full_input_cpp_init,
+                                lex_full_input_cpp_breaks,
+                                F4_MD_PosContext,
+                                F4_MD_Highlight,
+                                Lex_State_Cpp);
+        }
+    }
+    // NOTE(mb): Odin
+    {
+        F4_RegisterLanguage(S8Lit("odin"),
+                            MB_Odin_IndexFile,
+                            lex_full_input_odin_init,
+                            lex_full_input_odin_breaks,
+                            MB_Odin_PosContext,
+                            MB_Odin_Highlight,
+                            Lex_State_Odin);
+    }
+}
+
+#endif //FCODER_FLEURY_LANG_LIST_H
---
+++ build_lexers.bat
@@ -0,0 +1,10 @@
+@echo off
+call ..\bin\build_one_time .\4coder_fleury_jai_lexer_gen.cpp ..\..\
+..\..\one_time.exe
+copy ..\generated\lexer_jai.h .\generated\4coder_fleury_lexer_jai.h
+copy ..\generated\lexer_jai.cpp .\generated\4coder_fleury_lexer_jai.cpp
+
+call ..\bin\build_one_time .\4coder_marko_odin_lexer_gen.cpp ..\..\
+..\..\one_time.exe
+copy ..\generated\lexer_odin.h .\generated\4coder_marko_lexer_odin.h
+copy ..\generated\lexer_odin.cpp .\generated\4coder_marko_lexer_odin.cpp
